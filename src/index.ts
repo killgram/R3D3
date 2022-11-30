e818552c761f8c8f52692523e78a80691d006732
killgram <killgram94@gmail.com>
2022-11-30 17:02:34 +0600

    init project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+import cors from "cors";
+
+const app: Application = express();
+const PORT = process.env.PORT || 9987;
+import bodyParser from "body-parser";
+
+
+// configuration
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// GET
+
+// POST
+
+// listener
+app.listen(PORT, (): void => {
+    console.log(`R3D3 running on port here 👉 ${PORT}`);
+});
