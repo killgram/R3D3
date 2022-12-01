af7654de110169135ae86edc26f5d145051bb84f
killgram <killgram94@gmail.com>
2022-12-01 16:59:05 +0600

    remove socket

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Application } from "express";
 import cors from "cors";
-import { WebSocketServer } from "ws";
 
 const app: Application = express();
 const PORT = process.env.PORT || 9987;
@@ -23,14 +22,4 @@ app.get("/getSupportInfo", getSupportInfo);
 // listener
 app.listen(PORT, (): void => {
   console.log(`R3D3 running on port here 👉 ${PORT}`);
-});
-
-const wss = new WebSocketServer({ port: 9000 });
-
-wss.on("connection", function connection(ws) {
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
-
-  ws.send("something");
 });
